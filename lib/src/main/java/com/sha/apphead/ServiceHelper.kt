30b5ad3a450a489deb6f99d5a8319d4b7c7a0058
ShabanKamel <sh3ban.kamel@gmail.com>
2019-11-10 12:59:28 +0200

    merge Ibrahim Ali changes

--- lib/src/main/java/com/sha/apphead/ServiceHelper.kt
+++ lib/src/main/java/com/sha/apphead/ServiceHelper.kt
@@ -7,16 +7,14 @@ import android.content.Intent
 internal object ServiceHelper {
 
     fun start(clazz: Class<*>, context: Context) {
-        try {
+        runCatching {
             context.startService(Intent(context, clazz))
-        } catch (e: Exception) {
         }
     }
 
     fun stop(clazz: Class<*>, context: Context) {
-        try {
-            context.stopService(Intent(context, clazz))
-        } catch (e: Exception) {
+        runCatching {
+            context.startService(Intent(context, clazz))
         }
     }
 }
